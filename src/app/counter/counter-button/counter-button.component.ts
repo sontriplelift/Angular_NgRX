ace114421a7a7ce8d76956a7550bfe3f56cdd360
sontriplelift <sontriplelift@gmail.com>
2026-01-15 11:18:57 +0700

    dispatch actions and get state by store.select

--- src/app/counter/counter-button/counter-button.component.ts
+++ src/app/counter/counter-button/counter-button.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../states/counter.actions';
+import { CounterState } from '../states/counter.state';
 
 @Component({
   selector: 'app-counter-button',
   templateUrl: './counter-button.component.html',
-  styleUrls: ['./counter-button.component.css']
+  styleUrls: ['./counter-button.component.css'],
 })
 export class CounterButtonComponent {
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
+  onIncrement() {
+    this.store.dispatch(increment());
+  }
+
+  onDecrement() {
+    this.store.dispatch(decrement());
+  }
+
+  onReset() {
+    this.store.dispatch(reset());
+  }
 }
--- src/app/counter/counter-value/counter-value.component.ts
+++ src/app/counter/counter-value/counter-value.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { CounterState } from '../states/counter.state';
 
 @Component({
   selector: 'app-counter-value',
   templateUrl: './counter-value.component.html',
-  styleUrls: ['./counter-value.component.css']
+  styleUrls: ['./counter-value.component.css'],
 })
 export class CounterValueComponent {
-  count = 0;
+  counter = 0;
+  counterSubscription: Subscription | null = null;
+
+  constructor(private store: Store<{ counter: CounterState }>) {}
+
+  ngOnInit() {
+    this.counterSubscription = this.store
+      .select('counter')
+      .subscribe((data) => {
+        this.counter = data.counter;
+      });
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription?.unsubscribe();
+  }
 }
